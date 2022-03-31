c9674e7005a643f95faf297a6a8dfcd85efee0f9
Konteh <kontehebrima993@gmail.com>
2022-03-31 01:34:26 -0700

    create firebase

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCp0hsSFfLIBYrKI2cB2lcgGzzA7zsrfwE",
+  authDomain: "godashboard-86dbb.firebaseapp.com",
+  projectId: "godashboard-86dbb",
+  storageBucket: "godashboard-86dbb.appspot.com",
+  messagingSenderId: "434749904428",
+  appId: "1:434749904428:web:82239d6ffb5b7c97fb6389"
+};
+
+// Initialize Firebase
