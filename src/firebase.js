57d5586cdcd874d04b3dca9580d5ccf98baad482
Konteh <kontehebrima993@gmail.com>
2022-04-05 01:33:47 -0700

    firebase login authentication

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,6 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import {getAuth} from "firebase/auth"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -12,4 +14,8 @@ const firebaseConfig = {
   appId: "1:434749904428:web:82239d6ffb5b7c97fb6389"
 };
 
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth()
 // Initialize Firebase
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,13 +1,31 @@
-import { useState } from "react"
-import "./login.scss"
+import { useState } from "react";
+import "./login.scss";
+import {signInWithEmailAndPassword} from "firebase/auth";
+import {auth} from '../../firebase';
 
 const Login = () => {
-  const [error, setError] = useState(false)
+  const [error, setError] = useState(false);
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleLogin = (e) => {
+    e.preventDefault()
+
+    signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    console.log(user)
+  })
+  .catch((error) => {
+    setError(true)
+  });
+  }
   return (
     <div className="login">
-      <form>
-        <input type="email" placeholder="email"></input>
-        <input type="password" placeholder="password"></input>
+      <form onSubmit={handleLogin}>
+        <input type="email" placeholder="email" onChange={e=>setEmail(e.target.value)}></input>
+        <input type="password" placeholder="password" onChange={e=>setPassword(e.target.value)}></input>
         <button type="submit">Login</button>
         {error && <span>Wrong email or password</span>}
       </form>
