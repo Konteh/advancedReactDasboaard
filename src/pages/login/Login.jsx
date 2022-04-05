b02f4deefc815ffdc68ca5605065a6fa9dc35730
Konteh <kontehebrima993@gmail.com>
2022-04-04 23:24:03 -0700

    create the login form

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,8 +1,17 @@
+import { useState } from "react"
 import "./login.scss"
 
 const Login = () => {
+  const [error, setError] = useState(false)
   return (
-    <div>Login</div>
+    <div className="login">
+      <form>
+        <input type="email" placeholder="email"></input>
+        <input type="password" placeholder="password"></input>
+        <button type="submit">Login</button>
+        {error && <span>Wrong email or password</span>}
+      </form>
+    </div>
   )
 }
 
